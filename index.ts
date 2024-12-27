06f981d09f02273eba74114e089d89a2528eee7f
Albert Diones <adiones@addteam.org>
2024-12-27 16:26:13 +0800

    Revert "stash"
    This reverts commit 09171a74bcf0d1edf2afbf3a755c8a6708f4135f.

--- index.ts
+++ index.ts
@@ -1,36 +1,31 @@
 import { open } from "fs/promises";
 
 export async function getFileTail(filePath: string, linesToRead: number): Promise<string[]> {
-    const bufferSize = 512; // Size of the buffer to read chunks    
+    const bufferSize = 512; // Size of the buffer to read chunks
+    const fileHandle = await open(filePath, "r");
+    let fileSize = (await fileHandle.stat()).size;
+  
     const buffer = Buffer.alloc(bufferSize);
-    return open(filePath, "r").then(
-        (fileHandle) => {
-            fileHandle.stat().then(
-                ({size: fileSize}) => {
-                    let lines: string[] = [];
-                    let position = fileSize;
-                    let partialData = "";
-                
-                    try {
-                        while (lines.length <= linesToRead && position > 0) {
-                            const bytesToRead = Math.min(bufferSize, position);
-                            position -= bytesToRead;
-                    
-                            // Read the file chunk
-                            fileHandle.read(buffer, 0, bytesToRead, position);
-                            partialData = buffer.toString("utf-8", 0, bytesRead) + partialData;
-                    
-                            // Split lines and prepend them to the array
-                            lines = partialData.split("\n");
-                        }
-                    } finally {
-                        await fileHandle.close();
-                    }
-                
-                    // Return the last `linesToRead` lines
-                    return lines.slice(-linesToRead);
-                }
-            )
-        }
-    )
+    let lines: string[] = [];
+    let position = fileSize;
+    let partialData = "";
+  
+    try {
+      while (lines.length <= linesToRead && position > 0) {
+        const bytesToRead = Math.min(bufferSize, position);
+        position -= bytesToRead;
+  
+        // Read the file chunk
+        const { bytesRead } = await fileHandle.read(buffer, 0, bytesToRead, position);
+        partialData = buffer.toString("utf-8", 0, bytesRead) + partialData;
+  
+        // Split lines and prepend them to the array
+        lines = partialData.split("\n");
+      }
+    } finally {
+      await fileHandle.close();
+    }
+  
+    // Return the last `linesToRead` lines
+    return lines.slice(-linesToRead);
   }
--- test.ts
+++
@@ -1,4 +0,0 @@
-import { getFileTail } from ".";
-
-
-console.log(await getFileTail('logs/all.warn.txt',10));
