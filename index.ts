1ec9ee7f98599ac39a33528e74baad4a3e6ba6b3
Albert Diones <adiones@addteam.org>
2024-12-27 17:14:43 +0800

    removed concatenation of result, instead created an array of chunks

--- index.ts
+++ index.ts
@@ -1,17 +1,18 @@
 import { open } from "fs/promises";
 
-export async function getFileTail(filePath: string, linesToRead: number): Promise<string> {
+export async function getFileTail(filePath: string, linesDesired: number): Promise<string> {
     const bufferSize = 512; // Size of the buffer to read chunks
     const fileHandle = await open(filePath, "r");
     let fileSize = (await fileHandle.stat()).size;
   
     const buffer = Buffer.alloc(bufferSize);
     let position = fileSize;
-    let result = "";
+    const results: string[] = [];
+    let currentResultLines: number = 0;
   
     try {
       while (
-        (result.match(/\n/g) ?? []).length <= linesToRead 
+        currentResultLines < linesDesired 
         && position > 0
     ) {
         const bytesToRead = Math.min(bufferSize, position);
@@ -20,13 +21,27 @@ export async function getFileTail(filePath: string, linesToRead: number): Promis
         // Read the file chunk
         const { bytesRead } = await fileHandle
             .read(buffer, 0, bytesToRead, position);
-        result = buffer
-            .toString("utf-8", 0, bytesRead) + result;
+        
+        // new chunk of text read from the bottom
+        const chunk = buffer
+            .toString("utf-8", 0, bytesRead);
+        const chunkLines = (chunk.match(/\n/g) ?? []).length;
+        const lack = linesDesired - currentResultLines;
+        if (chunkLines > lack) {
+            results
+                .unshift(
+                    chunk.split('\n').slice(-lack).join('\n')
+                )
+            currentResultLines += lack;
+            continue;
+        }
+        currentResultLines += chunkLines;
+        results.unshift(chunk);
       }
     } finally {
       await fileHandle.close();
     }
   
     // Return the last `linesToRead` lines
-    return result.split('\n').slice(-linesToRead).join('\n');
+    return results.join("");
   }
