c95262fbc93eaa7dcd4e97cf9ca5c709911b6980
Albert Diones <adiones@addteam.org>
2024-12-28 17:59:39 +0800

    2nd test: test return all lines

--- tests/first.test.ts
+++ tests/first.test.ts
@@ -3,10 +3,19 @@ import { getFileTail } from '..';
 
 
 test(
-    'first test',
+    'test last 2 lines',
     async ()  => {
         const tail = await getFileTail('tests/assets/smol.txt',2);
         
         expect(tail.replace("\r","")).toBe("d\ne");
+    }
+);
+
+test(
+    'test all lines',
+    async ()  => {
+        const tail = await getFileTail('tests/assets/smol.txt',10);
+        
+        expect(tail.replace(/\r/g,"")).toBe("a\nb\nc\nd\ne");
     }
 );
